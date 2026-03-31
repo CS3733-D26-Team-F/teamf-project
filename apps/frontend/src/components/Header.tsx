b33790f256b0eb1723d4bf340bb2fe0fbf8fbd2b
a-little-black-cat <berenis.tekin@gmail.com>
2026-03-31 16:53:53 -0400

    name confusion solved

--- apps/frontend/src/components/Header.tsx
+++ apps/frontend/src/components/Header.tsx
@@ -1,12 +1,36 @@
 import { Link } from 'react-router-dom'
+import hanoverLogo from '../assets/hanoverlogo.png';
+
 
 export function Header() {
     return (
-            <nav className="menu">
-                <Link to="/">Home</Link> | {" "}
-                <Link to="/template">Template</Link> | {" "}
-                <Link to="/link2"> Link 2</Link> | {" "}
-                <Link to="/link3"> Link 3</Link>
-            </nav>
-    );
+            <>
+            <div className="menu">
+                <table className="menu-content">
+
+                    <thead>
+                        <tr>
+                            <th>
+                                <a href="/home"><img src={hanoverLogo} id="hanover-logo-menu"
+                                        alt="Hanover Insurance Logo"
+                                        style={{width: '90px'}}/></a></th>
+
+                            <th>The Hanover Insurance Group</th>
+                            <th id="menu-spacer"> </th>
+                            <th>
+                                <div className="menu-links">
+                                    <a href="/home"> Home</a>
+                                    <a href="/link1"> Link 1</a>
+                                    <a href="/link2"> Link 2</a>
+                                    <a href="/link3"> Link 3</a>
+                                </div>
+                            </th>
+                        </tr>
+                    </thead>
+                </table>
+
+
+            </div>
+            </>
+        );
 }
