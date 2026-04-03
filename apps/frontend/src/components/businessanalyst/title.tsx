f34f2d1fd8307d15171ac80a9f4a5e8dbb0459ca
Adrian <aakeem0920@gmail.com>
2026-04-02 23:18:48 -0400

    Make h1 selector more specific

--- apps/frontend/src/components/businessanalyst/title.tsx
+++ apps/frontend/src/components/businessanalyst/title.tsx
@@ -1,15 +1,7 @@
-import type {ReactNode} from 'react'
-
-export function TitleComponent(): ReactNode {
+export function TitleDemo() {
     return (
         <div className="text-yale-blue">
             <h1>Business Analyst Resources</h1>
         </div>
-    )
-}
-
-export function TitleDemo() {
-    return (
-        <TitleComponent />
-    )
+    );
 }
