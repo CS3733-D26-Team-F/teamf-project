ab57235a75020b35537b1bd84dcac7f03a51372b
Adrian <aakeem0920@gmail.com>
2026-04-02 22:53:22 -0400

    Creating hero carousel + code rewrites

--- apps/frontend/src/components/Header.tsx
+++ apps/frontend/src/components/Header.tsx
@@ -1,37 +1,21 @@
 import { Link } from 'react-router-dom'
 import hanoverLogo from '../assets/hanoverlogo.png';
 
-
 export function Header() {
     return (
-            <>
-            <div className="menu">
-                <table className="menu-content">
-
-                    <thead>
-                        <tr>
-                            <th>
-                                <Link to="/"><img src={hanoverLogo} id="hanover-logo-menu"
-                                        alt="Hanover Insurance Logo"
-                                        style={{width: '90px'}}/></Link></th>
-
-                            <th>The Hanover Insurance Group</th>
-                            <th id="menu-spacer"> </th>
-                            <th>
-                                <div className="menu-links">
-                                    <Link to="/"> Home</Link>
-                                    <Link to="/managecontent"> Manage Content</Link>
-                                    <Link to="/manageemployees"> Manage Employees</Link>
-                                    <Link to="/businessanalyst"> Business Analyst</Link>
-                                    <Link to="/corecommercialunderwriter"> Core Commercial Underwriter</Link>
-                                </div>
-                            </th>
-                        </tr>
-                    </thead>
-                </table>
-
-
+        <header className="menu">
+            <div className="logo">
+                <Link to="/">
+                    <img src={hanoverLogo} id="logo" alt="Hanover Insurance Logo" />
+                </Link>
             </div>
-            </>
-        );
+            <nav className="menu-links">
+                <Link to="/">Home</Link>
+                <Link to="/managecontent">Manage Content</Link>
+                <Link to="/manageemployees">Employees</Link>
+                <Link to="/businessanalyst">Business Analyst</Link>
+                <Link to="/corecommercialunderwriter">Core Commercial Underwriter</Link>
+            </nav>
+        </header>
+    );
 }
