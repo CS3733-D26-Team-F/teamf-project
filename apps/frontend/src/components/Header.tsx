5bc004ae80353e2b9f48a6ebf39d719064439e07
Adrian <aakeem0920@gmail.com>
2026-04-08 00:29:25 -0400

    Fix bug in header.tsx

--- apps/frontend/src/components/Header.tsx
+++ apps/frontend/src/components/Header.tsx
@@ -1,6 +1,8 @@
+import { useState, useEffect } from 'react';
+
 import { Link } from 'react-router-dom'
 import hanoverLogo from '../../public/main_icons/hanoverlogo.png';
-import ThemeToggle from "./ThemeToggle.tsx";
+//import ThemeToggle from "./ThemeToggle.tsx";
 
 export function Header() {
     const [theme, setTheme] = useState('default');
