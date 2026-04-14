fdd31c128d0d01e4407bb09caea1e064545bef6b
a-little-black-cat <berenis.tekin@gmail.com>
2026-04-14 16:21:00 -0400

    profileDisplayComponent.tsx

--- apps/frontend/src/pages/profilePage/profileDisplayComponent.tsx
+++ apps/frontend/src/pages/profilePage/profileDisplayComponent.tsx
@@ -11,22 +11,28 @@ export function ProfileComponent() {
     return (
         <div id="profile-component"
         
-        style={{ width:'80%', padding: '20px', alignContent: 'center', position: 'fixed', background:'white', borderRadius:12, boxShadow: '0 1px 4px rgba(0,0,0,0.1)', cursor: 'pointer',
+        style={{ width:'80%', margin: '0 auto', padding: '20px', alignContent: 'center', position: 'relative', background:'white', borderRadius:12, boxShadow: '0 2px 6px rgba(0,0,0,0.1)', cursor: 'pointer',
                 transition: 'box-shadow 0.15s', overflow: 'hidden', display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(450px, 1fr))', gap: 16 }}>
             
             <Table  style={{ padding: '20px' }}>
                 
-                <thead>
+                <thead id="profile-header" style={{ padding: '20px' }}>
                     <tr>
                         <th>{localStorage.getItem('first_name')}</th>
-                        <th>{localStorage.getItem('persona')}</th>
+                        <th>Role: {localStorage.getItem('persona')}</th>
 
                     </tr>
+                </thead>
+                <tbody>
                     <tr>
-                        <td><Image src={localStorage.getItem('pfp_URL') || placeholder} alt="Profile" /></td>
+                        <td><Image src={localStorage.getItem('pfp_URL') || placeholder} style={{ width: 200, height: 200 }} alt="Profile" /></td>
                         <td>Email: {localStorage.getItem('email')}</td>
                     </tr>
-                </thead>
+                    <tr>
+                        <td>Username: {localStorage.getItem('username')}</td>
+                        <td>Employee ID: {localStorage.getItem('empid')}</td>
+                    </tr>
+                </tbody>
 
             </Table>
         </div>
