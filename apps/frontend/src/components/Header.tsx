3a4e83113f7e3313f8dbfe2d49217d0f4a6bda63
chloepolit <chloepolit@gmail.com>
2026-04-27 20:07:07 -0400

    Fixed Link

--- apps/frontend/src/components/Header.tsx
+++ apps/frontend/src/components/Header.tsx
@@ -89,6 +89,7 @@ export function Header() {
                                         }}
                                     >{displayCount}</p>
                                 </div>
+                                )}
                             </Link>
                         </nav>
                     <Chatbot />
